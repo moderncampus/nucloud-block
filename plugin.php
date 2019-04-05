0081edbf6d454359c72667e7b6d89ae691ab1225
Michael Fienen <fienen@gmail.com>
2019-04-05 09:51:28 -0500

    Increment version number

--- plugin.php
+++ plugin.php
@@ -5,7 +5,7 @@
  * Description: Allows a user to embed their nuCloud powered interactive map into a page with a block in Gutenberg.
  * Author: Michael Fienen
  * Author URI: https://nucloud.com/
- * Version: .1
+ * Version: .2
  * License: GPL2+
  * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
  *
