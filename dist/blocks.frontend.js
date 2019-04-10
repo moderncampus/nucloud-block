79111dae0640a1689981e477b42f57630c343e03
Michael Fienen <fienen@gmail.com>
2019-04-10 10:15:41 -0500

    #2: Fix protocol for embedding and improve frame selector

--- dist/blocks.frontend.js
+++ dist/blocks.frontend.js
@@ -5,12 +5,12 @@ function getURLParameter(name) {
 window.onload = function() {
   var marker = getURLParameter('marker');
   var layer  = getURLParameter('layer');
-  var frame  = document.getElementById('nucloud-map');
+  var frame  = document.querySelector('.wp-block-nucloud-map-embed > iframe');
   var mapId  = frame.getAttribute('data-map-id');
 
   if(marker != null) {
-  	frame.src = 'http://cdn-map1.nucloud.com/nucloudmap/index.html?map=' + mapId + '&marker=' + marker;
+  	frame.src = '//cdn-map1.nucloud.com/nucloudmap/index.html?map=' + mapId + '&marker=' + marker;
   } else if(layer != null) {
-  	frame.src = 'http://cdn-map1.nucloud.com/nucloudmap/index.html?map=' + mapId + '&layer=' + layer;
+  	frame.src = '//cdn-map1.nucloud.com/nucloudmap/index.html?map=' + mapId + '&layer=' + layer;
   }
 }
