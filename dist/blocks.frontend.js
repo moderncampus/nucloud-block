1b8582f6ff843addc490d8f38bdf308e77360df7
Michael Fienen <fienen@gmail.com>
2019-11-08 13:35:28 -0600

    #21: Only try to set frame source if there's a map on the page

--- dist/blocks.frontend.js
+++ dist/blocks.frontend.js
@@ -1,17 +1,20 @@
 function getURLParameter(e){var r=(RegExp("[?|&]"+e+"=(.+?)(&|$)").exec(location.search)||[,null])[1];return r?escape(decodeURI(r)):escape(r)}
 
 window.addEventListener('DOMContentLoaded'), () => {
-  var frame  = document.querySelector('.wp-block-nucloud-map-embed > iframe');
-  var mapId  = frame.getAttribute('data-map-id');
-  var marker = getURLParameter('marker') ? getURLParameter('marker') : frame.getAttribute('data-marker');
-  var layer  = getURLParameter('layer') ? getURLParameter('layer') : frame.getAttribute('data-layer');
-  var src    = 'https://cdn-map1.nucloud.com/nucloudmap/index.html?map=' + mapId;
+  var frame  = document.querySelector('.wp-block-nucloud-map-embed > iframe') !== null;
 
-  if(marker != null) {
-  	src += '&marker=' + marker;
-  } else if(layer != null) {
-  	src += '&layer=' + layer;
+  if(frame) {
+    var mapId  = frame.getAttribute('data-map-id');
+    var marker = getURLParameter('marker') ? getURLParameter('marker') : frame.getAttribute('data-marker');
+    var layer  = getURLParameter('layer') ? getURLParameter('layer') : frame.getAttribute('data-layer');
+    var src    = 'https://cdn-map1.nucloud.com/nucloudmap/index.html?map=' + mapId;
+
+    if(marker != null) {
+    	src += '&marker=' + marker;
+    } else if(layer != null) {
+    	src += '&layer=' + layer;
+    }
+
+    frame.src = src;
   }
-
-  frame.src = src;
 });
