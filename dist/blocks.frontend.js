2a8ed5997387a5262de21bdf54cb5ccff53ceb28
Michael Fienen <fienen@gmail.com>
2019-11-08 13:33:10 -0600

    #21: Update page load function to use DOMContentLoaded

--- dist/blocks.frontend.js
+++ dist/blocks.frontend.js
@@ -1,10 +1,10 @@
 function getURLParameter(e){var r=(RegExp("[?|&]"+e+"=(.+?)(&|$)").exec(location.search)||[,null])[1];return r?escape(decodeURI(r)):escape(r)}
 
-window.onload = function() {
-  var marker = getURLParameter('marker');
-  var layer  = getURLParameter('layer');
+window.addEventListener('DOMContentLoaded'), () => {
   var frame  = document.querySelector('.wp-block-nucloud-map-embed > iframe');
   var mapId  = frame.getAttribute('data-map-id');
+  var marker = getURLParameter('marker') ? getURLParameter('marker') : frame.getAttribute('data-marker');
+  var layer  = getURLParameter('layer') ? getURLParameter('layer') : frame.getAttribute('data-layer');
   var src    = 'https://cdn-map1.nucloud.com/nucloudmap/index.html?map=' + mapId;
 
   if(marker != null) {
@@ -14,4 +14,4 @@ window.onload = function() {
   }
 
   frame.src = src;
-}
+});
