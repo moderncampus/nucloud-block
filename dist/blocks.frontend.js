13c0d6f0c6b099f1a0a2b8d7c94b111f2fc8bb7e
Michael Fienen <fienen@gmail.com>
2019-11-08 14:17:21 -0600

    #21: Fix ) typo

--- dist/blocks.frontend.js
+++ dist/blocks.frontend.js
@@ -1,6 +1,6 @@
 function getURLParameter(e){var r=(RegExp("[?|&]"+e+"=(.+?)(&|$)").exec(location.search)||[,null])[1];return r?escape(decodeURI(r)):escape(r)}
 
-window.addEventListener('DOMContentLoaded'), () => {
+window.addEventListener('DOMContentLoaded', () => {
   var frame  = document.querySelector('.wp-block-nucloud-map-embed > iframe') !== null;
 
   if(frame) {
